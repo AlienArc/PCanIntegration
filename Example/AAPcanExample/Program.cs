429b964dd32a19e25af24ca31685e65a2368805f
Duane Newman <duane@alienarc.com>
2018-10-04 14:00:24 -0500

    Added some example initialization

--- Example/AAPcanExample/Program.cs
+++ Example/AAPcanExample/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Peak.Can.Basic;
 
 namespace AAPcanExample
 {
@@ -10,7 +11,15 @@ namespace AAPcanExample
 	{
 		static void Main(string[] args)
 		{
-			
+			StringBuilder sb = new StringBuilder(5000);
+			var status = Peak.Can.Basic.PCANBasic.Initialize(81, TPCANBaudrate.PCAN_BAUD_250K);
+			Console.WriteLine(status.ToString());
+			status = Peak.Can.Basic.PCANBasic.GetValue(81, TPCANParameter.PCAN_HARDWARE_NAME, sb, (uint)sb.Capacity);
+			Console.WriteLine(status.ToString());
+			Console.WriteLine(sb.ToString());
+			status = Peak.Can.Basic.PCANBasic.Uninitialize(81);
+			Console.WriteLine(status.ToString());
+			Console.ReadKey();
 		}
 	}
 }
